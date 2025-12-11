877bb923b717d3a5e9cca7103ed06cbcf5bd2d8f
shaik shuaib ahmed <shuaib9845@gmail.com>
2025-12-11 17:18:56 +0530

    first commit

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ WORKDIR /app
 RUN apt-get update && apt-get install -y \
     gcc \
     g++ \
-    libgl1 \
+    libgl1-mesa-glx \
     libglib2.0-0 \
     libsm6 \
     libxext6 \
--- main.py
+++ main.py
@@ -22,9 +22,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000", 
-        "https://pickabook-ochre.vercel.app", ],
+    allow_origins=["http://localhost:3000", "https://pickabook-frontend-kappa.vercel.app/",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
